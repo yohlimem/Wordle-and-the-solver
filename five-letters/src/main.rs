c176c996c862d7401fc58c52773bfb2e9a82ad98
Yahli Mariansky <yahlimem@gmail.com>
2024-08-03 22:54:46 +0300

    fixed duplicates

--- five-letters/src/main.rs
+++ five-letters/src/main.rs
@@ -1,28 +1,63 @@
+use std::collections::HashSet;
 use std::fs;
 use std::io::Write;
 
-
 fn main() {
-    let words = fs::read_to_string("adjectives.txt").expect("Unable to read file");
-    let five_letter_words = words.lines().filter(|word| word.chars().count() == 5 && !word.contains('\'') && !word.contains('\"')).collect::<Vec<_>>().join("\n");
+    let mut seen = HashSet::new();
 
-    let mut five_letter_words_file = fs::File::create("five_letter_words.txt").expect("Unable to create file");
-    five_letter_words_file.write(five_letter_words.as_bytes()).expect("Unable to write data");
+    let words = fs::read_to_string("adjectives.txt").expect("Unable to read file");
+    let five_letter_words = words
+        .lines()
+        .filter(|word| word.chars().count() == 5 && !word.contains('\'') && !word.contains('\"'))
+        .filter(|word| seen.insert(word.to_string())) // Filter out duplicates
+        .collect::<Vec<_>>()
+        .join("\n");
+
+    let mut five_letter_words_file =
+        fs::File::create("five_letter_words.txt").expect("Unable to create file");
+    five_letter_words_file
+        .write(five_letter_words.as_bytes())
+        .expect("Unable to write data");
 
     let words = fs::read_to_string("nouns.txt").expect("Unable to read file");
-    let five_letter_words = words.lines().filter(|word| word.chars().count() == 5 && !word.contains('\'') && !word.contains('\"')).collect::<Vec<_>>().join("\n");
-    five_letter_words_file.write(five_letter_words.as_bytes()).expect("Unable to write data");
+    let five_letter_words = words
+        .lines()
+        .filter(|word| word.chars().count() == 5 && !word.contains('\'') && !word.contains('\"'))
+        .filter(|word| seen.insert(word.to_string())) // Filter out duplicates
+        .collect::<Vec<_>>()
+        .join("\n");
+    five_letter_words_file
+        .write(five_letter_words.as_bytes())
+        .expect("Unable to write data");
 
     let words = fs::read_to_string("prepositions.txt").expect("Unable to read file");
-    let five_letter_words = words.lines().filter(|word| word.chars().count() == 5 && !word.contains('\'') && !word.contains('\"')).collect::<Vec<_>>().join("\n");
-    five_letter_words_file.write(five_letter_words.as_bytes()).expect("Unable to write data");
+    let five_letter_words = words
+        .lines()
+        .filter(|word| word.chars().count() == 5 && !word.contains('\'') && !word.contains('\"'))
+        .filter(|word| seen.insert(word.to_string())) // Filter out duplicates
+        .collect::<Vec<_>>()
+        .join("\n");
+    five_letter_words_file
+        .write(five_letter_words.as_bytes())
+        .expect("Unable to write data");
 
     let words = fs::read_to_string("new_words.txt").expect("Unable to read file");
-    let five_letter_words = words.split(",")
-    .map(|word| word.trim_start_matches(" \'").trim_end_matches('\''))
-    .filter(|word| word.chars().count() == 5 && !word.contains('\"') && !word.contains("]") && !word.contains("[")).collect::<Vec<_>>().join("\n");
-    five_letter_words_file.write(five_letter_words.as_bytes()).expect("Unable to write data");
+    let five_letter_words = words
+        .split(",")
+        .map(|word| word.trim_start_matches(" \'").trim_end_matches('\''))
+        .filter(|word| {
+            word.chars().count() == 5
+                && !word.contains('\"')
+                && !word.contains("]")
+                && !word.contains("[")
+        })
+        .filter(|word| seen.insert(word.to_string())) // Filter out duplicates
+        .collect::<Vec<_>>()
+        .join("\n");
+    five_letter_words_file
+        .write(five_letter_words.as_bytes())
+        .expect("Unable to write data");
 
-    let words = fs::read_to_string("verbs.txt").expect("Unable to read file");
-    let five_letter_words = words.lines().filter(|word| word.chars().count() == 5 && !word.contains('\'') && !word.contains('\"')).collect::<Vec<_>>().join("\n");
+    // let words = fs::read_to_string("verbs.txt").expect("Unable to read file");
+    // let five_letter_words = words.lines().filter(|word| word.chars().count() == 5 && !word.contains('\'') && !word.contains('\"')).collect::<Vec<_>>().join("\n");
 }
