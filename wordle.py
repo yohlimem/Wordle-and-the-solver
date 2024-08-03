d189c8ceb8b90c6a5d15dc984db7649a8ca184e4
Yahli Mariansky <yahlimem@gmail.com>
2024-08-03 16:34:42 +0300

    dwadwad

--- wordle.py
+++ wordle.py
@@ -4,32 +4,32 @@ from get_random_word import get_random
 word = get_random_word.get_random()
 print(word)
 i = 0
+
+
 def guess(word_guess):
     global i
 
     while i < 6:
         word_try = word_guess
-        if (len(word_try) != 5 or word_try not in get_random_word.contents):
+        if len(word_try) != 5 or word_try not in get_random_word.contents:
             print("please enter a real 5 letter word")
             continue
         char_list = [*word_try]
         answer = []
         for n in range(5):
-            if ([*word][n] == char_list[n]):
+            if [*word][n] == char_list[n]:
                 answer.append(1)
-            elif (char_list[n] in [*word]):
+            elif char_list[n] in [*word]:
                 answer.append(0)
             else:
                 answer.append(-1)
 
-        if (answer ==[1,1,1,1,1]):
+        if answer == [1, 1, 1, 1, 1]:
             print("you won!")
             break
 
-
         i += 1
         return answer
-
 
 
 def guesser():
@@ -37,4 +37,6 @@ def guesser():
         word_guess = get_random()
         print(word_guess)
         print(guess(word_guess))
+
+
 guesser()
