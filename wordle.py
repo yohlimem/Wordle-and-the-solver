d12fb2e83a5658d97c83e0b1c14dbecfbcd23e9c
GRock3 <guyadar100@gmail.com>
2024-08-03 16:22:13 +0300

    added the guesser function to the wordle.py file

--- wordle.py
+++ wordle.py
@@ -2,28 +2,39 @@ import get_random_word
 from get_random_word import get_random
 
 word = get_random_word.get_random()
-
-i=0
 print(word)
-while i<6:
-    word_try = input("enter word:   ")
-    if(len(word_try)!=5 or word_try not in get_random_word.contents):
-        print("please enter a real 5 letter word")
-        continue
-    char_list = [*word_try]
-    output = ""
-    for n in range(5):
-        if([*word][n]==char_list[n]):
-            output +=" right,"
-        elif(char_list[n] in [*word]):
-            output += " half,"
-        else:
-            output += " wrong,"
+i = 0
+def guess(word_guess):
+    global i
 
-    if(output == " right, right, right, right, right,"):
-        print("you won!")
-        break
+    while i < 6:
+        word_try = word_guess
+        if (len(word_try) != 5 or word_try not in get_random_word.contents):
+            print("please enter a real 5 letter word")
+            continue
+        char_list = [*word_try]
+        answer = []
+        for n in range(5):
+            if ([*word][n] == char_list[n]):
+                answer.append(1)
+            elif (char_list[n] in [*word]):
+                answer.append(0)
+            else:
+                answer.append(-1)
 
-    print(output)
-    i+=1
-print("womp womp")
+        if (answer ==[1,1,1,1,1]):
+            print("you won!")
+            break
+
+
+        i += 1
+        return answer
+
+
+
+def guesser():
+    for i in range(6):
+        word_guess = get_random()
+        print(word_guess)
+        print(guess(word_guess))
+guesser()
