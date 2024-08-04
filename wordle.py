8f8dfadcc9fe0ef0ea62c5f5ae85717d65147101
GRock3 <guyadar100@gmail.com>
2024-08-04 13:55:30 +0300

    finished the main checks

--- wordle.py
+++ wordle.py
@@ -2,6 +2,7 @@ import contents
 import get_random_word
 import words
 from get_random_word import get_random
+import re
 
 word = ""
 i = 0
@@ -11,7 +12,8 @@ def start_game():
     global i
     global word
     i = 0
-    word = "משעול"
+    word = get_random_word.get_random()
+    #word="קלזיו"
     print(word)
 
 
@@ -76,28 +78,48 @@ def match(word, green_letters, yellow_letters, wrong_letters):
 
     return True
 
+def get_word_without_used_words(words,words_to_avoid):
+    if len(words_to_avoid) > len(words):
+        assert False, f"pattern is longer than word: {words_to_avoid} {words}"
+    if any(c in word for c in words_to_avoid):
+        return False
 
-def list_matches(correct_letters, all_words, yellow_letters, wrong_letters):
+def list_matches(correct_letters, all_words, yellow_letters, wrong_letters,guess_results,turn):
     # matches = []
     # for word in all_words:
     #     if match(word, pattern, yellow_letters, wrong_letters):
     #         matches.append(word)
+    print(guess_results)
+    if guess_results.count(1) == 4 and guess_results.count(-1) == 1 and turn<4:
+        print("run")
 
-    matches = filter(
-        lambda word: match(word, correct_letters, yellow_letters, wrong_letters),
-        all_words,
-    )
+        words_to_avoid = []
+
+        for i, j in correct_letters:
+            words_to_avoid.append(i)
+        for i, j in yellow_letters:
+            words_to_avoid.append(i)
+        words_to_avoid+=wrong_letters
+        newmatch = filter(lambda word: get_word_without_used_words(all_words, words_to_avoid), all_words)
+        print(words_to_avoid)
+        new_best_words, best_score = words.best_words(newmatch, words.average_letter_position)
+        print(new_best_words)
+        if(len(new_best_words)>0):
+
+            print("new word time!")
+            return new_best_words[0]
+
+    matches = filter(lambda word: match(word, correct_letters, yellow_letters, wrong_letters), all_words)
 
     best_words, best_score = words.best_words(matches, words.average_letter_position)
     return best_words[0]
-
 
 
 def guesser():
     start_game()
     yellow_letters = []
     wrong_letters = []
-    word_guess = "אחיהן"
+    word_guess = "והרלי"
 
     for i in range(5):
         green_letter = []
@@ -122,7 +144,7 @@ def guesser():
 
         if len(green_letter) > 0 or len(yellow_letters) > 0 or len(wrong_letters) > 0:
             words = list_matches(
-                green_letter, dictionary, yellow_letters, wrong_letters
+                green_letter, dictionary, yellow_letters, wrong_letters,guess_results,i
             )
         else:
 
@@ -139,12 +161,11 @@ def guesser():
 
 
 if __name__ == "__main__":
-    # guesser()
-    # correct = 0
-    # for i in range(100):
-    #     if guesser():
-    #         correct += 1
-    #     print("=========================================")
-    # print(correct / 100)
-    guesser()
-    
+   guesser()
+#     correct = 0
+#     for i in range(100):
+#         if guesser():
+#             correct += 1
+#         print("=========================================")
+#     print(correct / 100)
+
